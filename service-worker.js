00002b6527d5731eef5ae42ddba27a38e93f6596
村上正邦 <murakamimasakuni@iMac.local>
2026-03-07 17:08:02 +0900

    Deploy to GitHub Pages

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'pitch-trainer-v23';
-const VERSION = '23';
+const CACHE_NAME = 'pitch-trainer-v24';
+const VERSION = '24';
 const ASSETS = [
     './?v=' + VERSION,
     './index.html?v=' + VERSION,
